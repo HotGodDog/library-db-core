252cc38b7b6cab1ec9e4ded78d2809886948421a
Ivan <moskvinuwu@gmail.com>
2026-05-17 12:02:56 +0800

    Added reference models (Category, Publisher, Author)

--- src/library_db_core/__init__.py
+++ src/library_db_core/__init__.py
@@ -1,3 +1,6 @@
 """library-db-core: Core database library for library management system."""
 
+from .models import Author, Category, Publisher
+
 __version__ = "0.1.0"
+__all__ = ["Author", "Category", "Publisher"]
---
+++ src/library_db_core/models.py
@@ -0,0 +1,47 @@
+"""Data models for library database"""
+
+
+class Category:
+    """Book category/genre"""
+    
+    def __init__(self, name: str, description: str = "", category_id: int = None):
+        self.category_id = category_id
+        self.name = name
+        self.description = description
+    
+    def __repr__(self):
+        return f"Category({self.category_id}, '{self.name}')"
+
+
+class Publisher:
+    """Book publisher"""
+    
+    def __init__(self, name: str, city: str = "", country: str = "", publisher_id: int = None):
+        self.publisher_id = publisher_id
+        self.name = name
+        self.city = city
+        self.country = country
+    
+    def __repr__(self):
+        return f"Publisher({self.publisher_id}, '{self.name}')"
+
+
+class Author:
+    """Book author"""
+    
+    def __init__(self, last_name: str, first_name: str, middle_name: str = "", author_id: int = None):
+        self.author_id = author_id
+        self.last_name = last_name
+        self.first_name = first_name
+        self.middle_name = middle_name
+    
+    @property
+    def full_name(self) -> str:
+        """Return full name"""
+        parts = [self.last_name, self.first_name]
+        if self.middle_name:
+            parts.append(self.middle_name)
+        return " ".join(parts)
+    
+    def __repr__(self):
+        return f"Author({self.author_id}, '{self.full_name}')"
